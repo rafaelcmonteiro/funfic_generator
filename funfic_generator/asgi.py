69b511402eecf003c6f26ac45cf4d6eaa90cf4db
Rafael <rafael.cds.monteiro@hotmail.com>
2023-06-05 18:46:03 -0300

    chore: creating new django project.

--- funfic_generator/asgi.py
+++ funfic_generator/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for movie_mania project.
+ASGI config for funfic_generator project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_mania.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'funfic_generator.settings')
 
 application = get_asgi_application()
